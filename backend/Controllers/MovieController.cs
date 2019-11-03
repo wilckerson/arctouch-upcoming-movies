9cf8c74dad0a7bfe187d8d0bfce6e10b7b4b9dfc
Wilckerson Ganda <wilckerson@gmail.com>
2019-11-03 16:41:14 -0300

    Implementing memory cache

--- backend/Controllers/MovieController.cs
+++ backend/Controllers/MovieController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using ArcTouch.UpcomingMovies.Api.BLL;
+using ArcTouch.UpcomingMovies.Api.Models;
 using ArcTouch.UpcomingMovies.Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace ArcTouch.UpcomingMovies.Api.Controllers
 {
@@ -12,10 +15,12 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
     public class MovieController : Controller
     {
         private readonly MovieBLL movieBLL;
+        private readonly IMemoryCache cache;
 
-        public MovieController(IMovieService movieService)
+        public MovieController(IMovieService movieService, IMemoryCache cache)
         {
             movieBLL = new MovieBLL(movieService);
+            this.cache = cache;
         }
 
         [HttpGet]
@@ -23,8 +28,15 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
         {
             try
             {
-                var result = await movieBLL.GetMovies(query,page);
-                return Ok(result);
+                var cacheResult = await cache.GetOrCreateAsync($"getmovies_{page}_{Sanitize(query)}", (context) =>
+                {
+                    context.SetAbsoluteExpiration(TimeSpan.FromMinutes(3));
+                    context.SetPriority(CacheItemPriority.High);
+
+                    var result = movieBLL.GetMovies(query, page);
+                    return result;
+                });
+                return Ok(cacheResult);
             }
             catch (Exception ex)
             {
@@ -32,15 +44,20 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
             }
         }
 
-        
-
         [HttpGet("genres")]
         public async Task<IActionResult> GetMovieGenres()
         {
             try
             {
-                var result = await movieBLL.GetMovieGenres();
-                return Ok(result);
+                var cacheResult = await cache.GetOrCreateAsync("getmoviegenres", (context) =>
+                {
+                    context.SetAbsoluteExpiration(TimeSpan.FromMinutes(7));
+                    context.SetPriority(CacheItemPriority.High);
+
+                    var result = movieBLL.GetMovieGenres();
+                    return result;
+                });
+                return Ok(cacheResult);
             }
             catch (Exception ex)
             {
@@ -53,13 +70,31 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
         {
             try
             {
-                var result = await movieBLL.GetMovieDetails(id);
-                return Ok(result);
+                var cacheResult = await cache.GetOrCreateAsync($"getmoviedetails_{id}", (context) =>
+                {
+                    context.SetAbsoluteExpiration(TimeSpan.FromMinutes(3));
+                    context.SetPriority(CacheItemPriority.High);
+
+                    var result = movieBLL.GetMovieDetails(id);
+                    return result;
+                });
+                return Ok(cacheResult);
             }
             catch (Exception ex)
             {
                 return BadRequest(ex);
             }
+        }
+
+        private static string Sanitize(string s)
+        {
+            if (string.IsNullOrEmpty(s)) { return null; }
+
+            return String.Join("", s.AsEnumerable()
+                                    .Select(chr => Char.IsLetter(chr) || Char.IsDigit(chr)
+                                                   ? chr.ToString()      // valid symbol
+                                                   : "_" + (short)chr + "_") // numeric code for invalid symbol
+                              );
         }
     }
 }
--- backend/Startup.cs
+++ backend/Startup.cs
@@ -40,6 +40,8 @@ namespace ArcTouch.UpcomingMovies.Api
             {
                 options.AddPolicy("MyPolicy", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             });
+
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
