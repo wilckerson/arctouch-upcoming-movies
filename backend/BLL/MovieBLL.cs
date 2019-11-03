6bc0316f9399bc9dfdfea3a24a7ff1e5380ecc51
Wilckerson Ganda <wilckerson@gmail.com>
2019-11-03 17:38:19 -0300

    Cache configuration parameters

--- backend/BLL/MovieBLL.cs
+++ backend/BLL/MovieBLL.cs
@@ -1,5 +1,7 @@
 ﻿using ArcTouch.UpcomingMovies.Api.Models;
 using ArcTouch.UpcomingMovies.Api.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,37 +11,80 @@ namespace ArcTouch.UpcomingMovies.Api.BLL
 {
     public class MovieBLL
     {
+        
+        private readonly IConfiguration configuration;
         private readonly IMovieService movieService;
+        private readonly IMemoryCache cacheService;
 
-        public MovieBLL(IMovieService movieService)
+        public MovieBLL(IMovieService movieService, IMemoryCache cacheService, IConfiguration configuration)
         {
             this.movieService = movieService;
+            this.cacheService = cacheService;
+            this.configuration = configuration;
         }
 
         public async Task<PaginatedMovieList> GetMovies(string query = null, int? page = null)
         {
-            if (!string.IsNullOrEmpty(query))
+            var cacheResult = await cacheService.GetOrCreateAsync($"getmovies_{page}_{Sanitize(query)}", (context) =>
             {
-                var result = await movieService.SearchMovies(query, page);
-                return result;
-            }
-            else
-            {
-                var result = await movieService.GetUpcomingMovies(page);
-                return result;
-            }
+                var minutes = configuration.GetSection("CacheExpiration").GetValue<int>("GetMoviesMinutes");
+                context.SetAbsoluteExpiration(TimeSpan.FromMinutes(minutes));
+                context.SetPriority(CacheItemPriority.High);
+
+                if (!string.IsNullOrEmpty(query))
+                {
+                    var result = movieService.SearchMovies(query, page);
+                    return result;
+                }
+                else
+                {
+                    var result = movieService.GetUpcomingMovies(page);
+                    return result;
+                }
+            });
+
+            return cacheResult;            
         }
 
         public async Task<Movie> GetMovieDetails(int id)
         {
-            var result = await movieService.GetMovieDetails(id);
-            return result;
+            var cacheResult = await cacheService.GetOrCreateAsync($"getmoviedetails_{id}", (context) =>
+            {
+                var minutes = configuration.GetSection("CacheExpiration").GetValue<int>("GetMovieDetailsMinutes");
+                context.SetAbsoluteExpiration(TimeSpan.FromMinutes(minutes));
+                context.SetPriority(CacheItemPriority.High);
+
+                var result = movieService.GetMovieDetails(id);
+                return result;
+            });
+
+            return cacheResult;
         }
 
         public async Task<IEnumerable<MovieGenre>> GetMovieGenres()
         {
-            var result = await movieService.GetMovieGenres();
-            return result;
+            var cacheResult = await cacheService.GetOrCreateAsync("getmoviegenres", (context) =>
+            {
+                var minutes = configuration.GetSection("CacheExpiration").GetValue<int>("GetGenresMinutes");
+                context.SetAbsoluteExpiration(TimeSpan.FromMinutes(minutes));
+                context.SetPriority(CacheItemPriority.High);
+
+                var result = movieService.GetMovieGenres();
+                return result;                
+            });
+
+            return cacheResult;
+        }
+
+        private static string Sanitize(string s)
+        {
+            if (string.IsNullOrEmpty(s)) { return null; }
+
+            return String.Join("", s.AsEnumerable()
+                                    .Select(chr => Char.IsLetter(chr) || Char.IsDigit(chr)
+                                                   ? chr.ToString()      // valid symbol
+                                                   : "_" + (short)chr + "_") // numeric code for invalid symbol
+                              );
         }
     }
 }
--- backend/Controllers/MovieController.cs
+++ backend/Controllers/MovieController.cs
@@ -8,6 +8,7 @@ using ArcTouch.UpcomingMovies.Api.Models;
 using ArcTouch.UpcomingMovies.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 
 namespace ArcTouch.UpcomingMovies.Api.Controllers
 {
@@ -15,12 +16,10 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
     public class MovieController : Controller
     {
         private readonly MovieBLL movieBLL;
-        private readonly IMemoryCache cache;
 
-        public MovieController(IMovieService movieService, IMemoryCache cache)
+        public MovieController(IMovieService movieService, IMemoryCache cacheService, IConfiguration configuration)
         {
-            movieBLL = new MovieBLL(movieService);
-            this.cache = cache;
+            movieBLL = new MovieBLL(movieService, cacheService, configuration);
         }
 
         [HttpGet]
@@ -28,15 +27,8 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
         {
             try
             {
-                var cacheResult = await cache.GetOrCreateAsync($"getmovies_{page}_{Sanitize(query)}", (context) =>
-                {
-                    context.SetAbsoluteExpiration(TimeSpan.FromMinutes(3));
-                    context.SetPriority(CacheItemPriority.High);
-
-                    var result = movieBLL.GetMovies(query, page);
-                    return result;
-                });
-                return Ok(cacheResult);
+                var result = await movieBLL.GetMovies(query, page);
+                return Ok(result);
             }
             catch (Exception ex)
             {
@@ -49,15 +41,8 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
         {
             try
             {
-                var cacheResult = await cache.GetOrCreateAsync("getmoviegenres", (context) =>
-                {
-                    context.SetAbsoluteExpiration(TimeSpan.FromMinutes(7));
-                    context.SetPriority(CacheItemPriority.High);
-
-                    var result = movieBLL.GetMovieGenres();
-                    return result;
-                });
-                return Ok(cacheResult);
+                var result = await movieBLL.GetMovieGenres();
+                return Ok(result);
             }
             catch (Exception ex)
             {
@@ -70,31 +55,13 @@ namespace ArcTouch.UpcomingMovies.Api.Controllers
         {
             try
             {
-                var cacheResult = await cache.GetOrCreateAsync($"getmoviedetails_{id}", (context) =>
-                {
-                    context.SetAbsoluteExpiration(TimeSpan.FromMinutes(3));
-                    context.SetPriority(CacheItemPriority.High);
-
-                    var result = movieBLL.GetMovieDetails(id);
-                    return result;
-                });
-                return Ok(cacheResult);
+                var result = await movieBLL.GetMovieDetails(id);
+                return Ok(result);
             }
             catch (Exception ex)
             {
                 return BadRequest(ex);
             }
-        }
-
-        private static string Sanitize(string s)
-        {
-            if (string.IsNullOrEmpty(s)) { return null; }
-
-            return String.Join("", s.AsEnumerable()
-                                    .Select(chr => Char.IsLetter(chr) || Char.IsDigit(chr)
-                                                   ? chr.ToString()      // valid symbol
-                                                   : "_" + (short)chr + "_") // numeric code for invalid symbol
-                              );
         }
     }
 }
